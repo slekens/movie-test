6f54ef33c2b1ffe3dc6378dce2e1b0b59e68214e
Abraham Abreu <slekens@Abrahams-MacBook-Pro.local>
2021-06-02 19:17:18 -0500

    First commit

---
+++ Movies/Movies/MoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesApp.swift
+//  Movies
+//
+//  Created by Abraham Abreu on 02/06/21.
+//
+
+import SwiftUI
+
+@main
+struct MoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
